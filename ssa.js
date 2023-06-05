ceca3cf720ae4d6adb176069f77ca39adb171c1e
yiqian987 <yiqian987@hotmail.com>
2023-06-05 16:17:54 +0800

    Update ssa.js

--- ssa.js
+++ ssa.js
@@ -1,3 +1,4 @@
+/*************************************
 #!name=SSA丝社
 
 #!desc=SSA丝社解锁会员
@@ -7,16 +8,14 @@
 #!homepage=https://github.com/deezertidal
 
 #!author=lutqhysky
-
-
-
+**************************************
 
 [rewrite_local]
 ^https?://www\.ssalegs\.store/cms/Appapi/username/username/\d+ url script-response-body  https://raw.githubusercontent.com/yiqian987/quanx/main/ssa.js
-
 [mitm]
 hostname = www.ssalegs.store
 
+*************************************/
 
 var body = $response.body.replace(/groupid":\d+/g, 'groupid":4').replace(/jomkenylv":"\d+"/g, 'jomkenylv":"5"').replace(/overduedate":\d+/g, 'overduedate": 4100726753').replace(/overduedate2":\d+/g, 'overduedate2": 4100726753').replace(/groupname":".*?"/g, 'groupname":"官方认证"').replace(/username":".*?"/g, 'username":"清清情"');
 $done({body});
