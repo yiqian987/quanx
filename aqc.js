084e3003f5c04c4de311b1f8d9a5e1179e21bae7
yiqian987 <yiqian987@hotmail.com>
2023-06-05 16:29:42 +0800

    Create aqc.js

---
+++ aqc.js
@@ -0,0 +1,19 @@
+/*************************************
+#!name=爱企查
+
+#!desc=爱企查vip
+
+#!homepage=https://github.com/deezertidal
+
+#!author=
+
+#!icon=https://raw.githubusercontent.com/deezertidal/private/main/icons/aqc.png
+**************************************
+[rewrite_local]
+^https://aiqicha.baidu.com/usercenter/getvipinfoajax url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/aiqicha.js
+[mitm]
+hostname = aiqicha.baidu.com
+*************************************/
+var body = $response.body
+    .replace(/\"svip\":\{\"status\":0,\"startTime\":\"\",\"endTime\":\"\"/, "\"svip\":\{\"status\":1,\"startTime\":\"\",\"endTime\":\"2099-12-31\"");
+$done({ body });
