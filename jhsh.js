efa55d67f856aabf71b3639f21f6a64cf5e73bce
yiqian987 <yiqian987@hotmail.com>
2023-06-07 10:02:23 +0800

    Create jhsh.js

---
+++ jhsh.js
@@ -0,0 +1,22 @@
+/*************************************
+
+项目名称: 建行生活
+
+**************************************
+
+[rewrite_local]
+^https?://yunbusiness\.ccb\.com/(clp_service|clp_coupon)/txCtrl\?.* url script-response-body https://raw.githubusercontent.com/yiqian987/quanx/main/jhsh.js
+[mitm]
+hostname = yunbusiness.ccb.com
+
+*************************************/
+
+
+var body = $response.body;
+
+
+body = body.replace(/\"SYSTEM_TIME":"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/g", '\"SYSTEM_TIME": "2023-06-08 18:59:59"');
+
+body = body.replace(/\"intervalUsedCount":\d+/g, '\"intervalUsedCount":1');
+
+$done({body});
