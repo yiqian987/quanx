1a77ff48b564ec44b408a57ab3db95a9485b739d
yiqian987 <yiqian987@hotmail.com>
2024-01-11 22:46:18 +0800

    Update wacai.js

--- wacai.js
+++ wacai.js
@@ -6,10 +6,10 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/jz\.wacaijizhang\.com url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/wacaijizhang.js
+^https?:\/\/jz\.wacaijizhang\.com url script-response-body https://raw.githubusercontent.com/yiqian987/quanx/main/wacai.js
 
 [mitm]
-hostname = jz.wacai.com
+hostname = jz.wacaijizhang.com
 
 *************************************/
 
