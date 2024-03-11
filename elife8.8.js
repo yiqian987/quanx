df7991b4c2f345293a6f8aca29874434f5336504
yiqian987 <yiqian987@hotmail.com>
2024-03-11 10:35:56 +0800

    Create elife8.8.js

---
+++ elife8.8.js
@@ -0,0 +1,25 @@
+/*************************************
+
+项目名称：最红星期五
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/market-web\.ofpay\.com url script-response-body https://raw.githubusercontent.com/yiqian987/quanx/main/elife8.8.js
+
+[mitm]
+hostname = market-web.ofpay.com
+
+*************************************/
+
+
+var body = $response.body;
+
+body = body.replace(/\"remainStock":\d+/g, '\"remainStock":100');
+
+body = body.replace(/\"buttonType":"\d+"/g, '\"buttonType":"01"');
+
+body = body.replace(/\"productActStatus":"\d+"/g, '\"productActStatus":""');
+
+$done({body});
