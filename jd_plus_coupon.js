6bca6830fea015486330050b98c764c48b35bee6
yiqian987 <yiqian987@hotmail.com>
2024-08-06 17:29:52 +0800

    Update jd_plus_coupon.js
    add wyw

--- jd_plus_coupon.js
+++ jd_plus_coupon.js
@@ -5,7 +5,7 @@
 **************************************
 
 [rewrite_local]
-^https?://api\.m\.jd\.com/api\?functionId=coupon_dayCouponList_v2.* url script-response-body https://raw.githubusercontent.com/yiqian987/quanx/main/jd_plus_coupon.js
+^https?://api\.m\.jd\.com/(api\?functionId=coupon_dayCouponList_v2.*|client.action) url script-response-body https://raw.githubusercontent.com/yiqian987/quanx/main/jd_plus_coupon.js
 [mitm]
 hostname = api.m.jd.com
 
@@ -18,5 +18,9 @@ var body = $response.body;
 body = body.replace(/\"couponState":\d+/g, '\"couponState":1');
 
 body = body.replace(/\"intervalUsedCount":\d+/g, '\"intervalUsedCount":1');
+
+body = body.replace(/\"exchangeStatus":\d+/g, '\"exchangeStatus":1');
+
+body = body.replace(/\"hasStock":false/g, '\"hasStock":true');
 
 $done({body});
