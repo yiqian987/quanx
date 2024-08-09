e8e162488f1ce1a0301de6d4a80d2a010b02aaef
yiqian987 <yiqian987@hotmail.com>
2024-08-09 16:54:38 +0800

    Create cbs.js

---
+++ cbs.js
@@ -0,0 +1,19 @@
+/*************************************
+
+项目名称: 长白山
+
+**************************************
+
+[rewrite_local]
+^https?://yun\.aichangbaishan\.com/platform/api\?method=ih\.cticket\.info\.getRestTickets url script-response-body https://raw.githubusercontent.com/yiqian987/quanx/main/cbs.js
+[mitm]
+hostname = aichangbaishan.com
+
+*************************************/
+
+
+var body = $response.body;
+
+body = body.replace(/\"number":\s\d+,/g, '\"number": 100,');
+
+$done({body});
