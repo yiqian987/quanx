859be81e157af980916c86e242e63d2e26e29aed
yiqian987 <yiqian987@hotmail.com>
2024-08-23 14:18:32 +0800

    Update pahcz.js

--- pahcz.js
+++ pahcz.js
@@ -6,7 +6,7 @@
 * @supported Quantumult X (v1.0.3-build141)
 */
 
-const myStatus = "HTTP/1.1 200";
+const myStatus = "HTTP/1.1 200 OK";
 const myHeaders = {"Location": "https://www.baidu.com"};
 const myData = "Here we go.";
 
