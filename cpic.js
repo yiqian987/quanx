989ce77649f26acd50b891436fcf653e51b6bbe4
MingJun Yu <20719472+yiqian987@users.noreply.github.com>
2025-07-29 16:49:52 +0800

    Update cpic.js

--- cpic.js
+++ cpic.js
@@ -15,7 +15,7 @@ hostname = cxbjwxsj.cpic.com.cn
 
 var body = $response.body;
 
-body = body.replace(/\"status":"\d"/g, '\"status":"1"');
+body = body.replace(/\"status":"\d"/g, '\"status": "1"');
 
 
 $done({body});
