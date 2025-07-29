dcf127ee88dabb59ed751ea025aedfecfc0064fe
MingJun Yu <20719472+yiqian987@users.noreply.github.com>
2025-07-29 16:48:48 +0800

    Update cpic.js

--- cpic.js
+++ cpic.js
@@ -16,6 +16,6 @@ hostname = cxbjwxsj.cpic.com.cn
 var body = $response.body;
 
 body = body.replace(/\"status":"\d"/g, '\"status":"1"');
-body = body.replace(/"surplusFlag"\s*:\s*(?:"N"|null)/g, '"surplusFlag":"Y"');
+
 
 $done({body});
