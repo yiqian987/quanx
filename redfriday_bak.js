fac0156b8a1cc67ff528f0b9aa7330e84c5d7baf
MingJun Yu <20719472+yiqian987@users.noreply.github.com>
2026-09-11 13:40:36 +0800

    Create redfriday_bak.js

---
+++ redfriday_bak.js
@@ -0,0 +1,31 @@
+/*************************************
+
+项目名称：最红星期五
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/creditcardapp\.bankcomm\.com url script-response-body https://raw.githubusercontent.com/yiqian987/quanx/main/redfriday.js
+
+[mitm]
+hostname = creditcardapp.bankcomm.com
+
+*************************************/
+
+
+var body = $response.body;
+
+body = body.replace(/\"reddestActCount":\d+/g, '\"reddestActCount":1');
+
+body = body.replace(/\"buttonType":"\d+"/g, '\"buttonType":"01"');
+
+body = body.replace(/\"productActStatus":"\d+"/g, '\"productActStatus":""');
+
+body = body.replace(/value="\d+"/g, 'value="01"');
+
+body = body.replace(/currentTm = (""|"\d:\d{1,2}:\d{1,2}")/g, 'currentTm = "11:00:00"');
+
+body = body.replace(/\"currentTm":"\d{1,2}:\d{2}:\d{2}"/g, '\"currentTm":"11:00:00"');
+
+$done({body});
