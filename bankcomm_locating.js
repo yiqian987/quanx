9eccc0eca0f37ba17dd59a8f0d5e11af47ff758e
MingJun Yu <20719472+yiqian987@users.noreply.github.com>
2026-09-14 10:12:59 +0800

    bankcomm_locating v9: split 930-char monster line into short concatenations (QuanX resource parser line-length limit)

--- bankcomm_locating.js
+++ bankcomm_locating.js
@@ -19,8 +19,41 @@ var LNG = '116.50666870332198';
 
 var referer = ($request.url.match(/referer=([^&]*)/) || [])[1] || '';
 var back = referer ? decodeURIComponent(referer) : '/catering/index.html';
-var target = /^https?:/i.test(back) ? back : 'https://creditcardapp.bankcomm.com' + (back.indexOf('/') === 0 ? back : '/' + back);
 
-var html = '<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1"><title>正在定位</title></head><body style="font-family:-apple-system;background:#f7f7f7;text-align:center;padding-top:40vh;color:#888;font-size:15px"><div>正在定位，请稍候…</div><scr' + 'ipt>var t=' + JSON.stringify(target) + ';var p={selCityNo:' + JSON.stringify(CITY_NO) + ',selCityName:' + JSON.stringify(CITY_NAME) + ',cityCode:' + JSON.stringify(CITY_NO) + ',cityName:' + JSON.stringify(CITY_NAME) + ',lat:' + JSON.stringify(LAT) + ',lng:' + JSON.stringify(LNG) + '};fetch("/catering/api/user/location.json?_="+Date.now(),{method:"POST",headers:{"Content-Type":"application/json;charset=utf-8"},body:JSON.stringify(p)}).then(function(){location.href=t;}).catch(function(){location.href=t;});setTimeout(function(){location.href=t;},4000);</scr' + 'ipt></body></html>';
+var target = back;
+if (!/^https?:/i.test(target)) {
+  if (target.charAt(0) !== '/') {
+    target = '/' + target;
+  }
+  target = 'https://creditcardapp.bankcomm.com' + target;
+}
+
+var payload = JSON.stringify({
+  selCityNo: CITY_NO,
+  selCityName: CITY_NAME,
+  cityCode: CITY_NO,
+  cityName: CITY_NAME,
+  lat: LAT,
+  lng: LNG
+});
+
+var js = 'var t=' + JSON.stringify(target) + ';'
+  + 'var p=' + payload + ';'
+  + 'fetch("/catering/api/user/location.json?_="+Date.now(),{'
+  + 'method:"POST",'
+  + 'headers:{"Content-Type":"application/json;charset=utf-8"},'
+  + 'body:JSON.stringify(p)'
+  + '}).then(function(){location.href=t;})'
+  + '.catch(function(){location.href=t;});'
+  + 'setTimeout(function(){location.href=t;},4000);';
+
+var css = 'font-family:-apple-system;background:#f7f7f7;'
+  + 'text-align:center;padding-top:40vh;color:#888;font-size:15px';
+
+var html = '<!DOCTYPE html><html><head><meta charset="utf-8">'
+  + '<meta name="viewport" content="width=device-width,initial-scale=1">'
+  + '<title>Locating</title></head><body style="' + css + '">'
+  + '<div>正在定位，请稍候…</div>'
+  + '<scr' + 'ipt>' + js + '</scr' + 'ipt></body></html>';
 
 $done({ body: html });
